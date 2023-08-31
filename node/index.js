c68966d4f51153bbb7e0c914222057c9150dfe92
Lucas Alves <lucas.arnaldo@gmail.com>
2023-08-31 19:56:31 -0300

    Add select from mysql db

--- node/index.js
+++ node/index.js
@@ -1,24 +1,48 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs');
 const port = 3000
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database:'nodedb'
-// };
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
 
-// const sql = `INSERT INTO people(name) values('Wesley')`
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+// const sql = `INSERT INTO people(name) values('Lucas')`
 // connection.query(sql)
 // connection.end()
 
+// /// TESTE
+// connection.query(sql_select, function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result);
+//     console.log('test')
+//     const data = JSON.stringify(result)
+// });
+// connection.end()
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Node</h1>')
+
+app.get('/', (req, res) => {
+    const sql_select = `SELECT * FROM people`
+    connection.query(sql_select, function (err, result, fields) {
+        if (err) throw err;
+    console.log(result)
+    console.log(result[0])
+    console.log(result[0]["id"])
+    
+    res.render('data_table.ejs', { data: result });
+    });
+    connection.end()
+    
+    // res.send('<h1>Full Cycle Node</h1>')
+    // res.send(data)
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('Rodando na porta ' + port)
+    console.log('Esperando')
 })
--- readme.md
+++ readme.md
@@ -7,6 +7,13 @@ Como rodar:
 ```
 docker-compose up -d
 ```
+
+--- 
+Duvidas:
+1 - Para o funcionar eu tive que adicionar no docker-compose do node port: 3000:3000 para mim isso não deveria acontecer pois o intuito é justamente o nginx fazer esse meio de campo. Gostaria de entender o pq disso.
+2 - Para instalar o dockerize precisei trocar a imagem do node:15 para node:latest, porem ao fazer isso, o nginx parou de enxergar  node. Portanto nessa entrega acabei comentando/regredindo o processo e enviando sem o dockerize. Gostaria de entender o pq disso...
+---
+
 Desafio Nginx com Node.js:
 ----
 Nesse desafio você colocará em prática o que aprendemos em relação a utilização do nginx como proxy reverso. A idéia principal é que quando um usuário acesse o nginx, o mesmo fará uma chamada em nossa aplicação node.js. Essa aplicação por sua vez adicionará um registro em nosso banco de dados mysql, cadastrando um nome na tabela people.
